1b91381cdfd775721980c7ae2039ba96d114520e
John-Arvid Kibsgård <john-arvidkibsgard@John-Arvids-MacBook-Pro.local>
2015-05-03 20:00:38 +0200

    git script

---
+++ git/github.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+clear
+
+#Install git
+yum install git -y
+
+#Create folder and clone git
+mkdir /root/scripts
+cd /root/scripts
+git clone https://github.com/john-arvid/centos-6.6.git
+
+
