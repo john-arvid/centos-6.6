b891ef74e31c6525c2937c362ea6cbbf2f37c19f
john-arvid <jakibsgaard@gmail.com>
2015-05-15 07:32:28 +0200

    Update github.sh
    echo global config as reminder

--- git/github.sh
+++ git/github.sh
@@ -10,4 +10,6 @@ mkdir /root/scripts
 cd /root/scripts
 git clone https://github.com/john-arvid/centos-6.6.git
 
-
+echo "Run this with your information"
+echo "git config –-global user.name Your Name"
+echo "git config –-global user.email youremail@mailsite.com"
