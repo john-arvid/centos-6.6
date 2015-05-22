962db76773e4fabc24055e1982d633a1f1b984cc
john-arvid <jakibsgaard@gmail.com>
2015-05-22 12:10:23 +0200

    Update config_sensu.sh

--- sensu/config_sensu.sh
+++ sensu/config_sensu.sh
@@ -1,18 +1,19 @@
 !#/bin/bash
 
-yum install -y gcc ruby ruby-devel
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+curl -L get.rvm.io | bash -s stable
+source /etc/profile.d/rvm.sh
+rvm reload
+rvm requirements run
+rvm install 2.2.2
 
-cd /tmp/
-wget http://production.cf.rubygems.org/rubygems/rubygems-2.4.7.tgz
-tar zxvf rubygems-2.4.7.tgz
-cd rubygems-2.4.7.tgz
-ruby setup.rb
-cd..
-rm -rf rubygems*
 
 gem update --system
 
 gem install sensu-plugin
+
+wget -O /etc/sensu/plugins/check-procs.rb http://sensuapp.org/docs/0.18/files/check-procs.rb
+chmod +x /etc/sensu/plugins/check-procs.rb
 
 cat <<EOT >> /etc/sensu/conf.d/check_cron.json
 {
@@ -28,6 +29,27 @@ cat <<EOT >> /etc/sensu/conf.d/check_cron.json
 }
 EOT
 
+wget -O /etc/sensu/plugins/cpu-metrics.rb http://sensuapp.org/docs/0.18/files/cpu-metrics.rb
+chmod +x /etc/sensu/plugins/cpu-metrics.rb
+
+cat <<EOT >> /etc/sensu/conf.d/cpu_metrics.json
+{
+  "checks": {
+    "cpu_metrics": {
+      "type": "metric",
+      "command": "/etc/sensu/plugins/cpu-metrics.rb",
+      "subscribers": [
+        "production"
+      ],
+      "interval": 10,
+      "handler": "debug"
+    }
+  }
+}
+EOT
+
+wget -O /etc/sensu/plugins/check-data.rb http://sensuapp.org/docs/0.18/files/check-data.rb
+chmod +x /etc/sensu/plugins/check-data.rb
 
 
 
