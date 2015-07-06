871f93042ec8e2ecc90f991f7cccf28712df406e
john-arvid <jakibsgaard@gmail.com>
2015-07-06 12:32:01 +0200

    Update lamp.sh

--- LAMP/lamp.sh
+++ LAMP/lamp.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+yum install httpd mysql-server php php-mysql -y
+
+service httpd start
+service mysqld start
+
+chkconfig httpd on
+chkconfig mysqld on
+
+/usr/bin/mysql_secure_installation
